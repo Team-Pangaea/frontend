f00033bb18b38043925c55569f180deaba832513
chavokim <treeboy0384@gmail.com>
2023-03-19 01:03:36 +0900

    testing mint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, useCallback, useEffect, useState} from 'react';
-import {web3Accounts, web3Enable} from "@polkadot/extension-dapp";
+import {web3Accounts, web3Enable, web3FromSource} from "@polkadot/extension-dapp";
 import {InjectedAccountWithMeta} from "@polkadot/extension-inject/types";
 import {useAccountStore} from "src/modules/AccountStore";
 import {ApiPromise, WsProvider} from "@polkadot/api";
@@ -59,9 +59,7 @@ function App() {
           "Content-Type": "multipart/form-data",
         },
       });
-
-      const ImgHash = JSON.stringify(`ipfs://${resFile.data.IpfsHash}`);
-      console.log(ImgHash);
+      
       //Take a look at your Pinata Pinned section, you will see a new file added to you list.
       setInput(prev => ({
         ...prev,
@@ -76,45 +74,43 @@ function App() {
   };
   
   const handleMint = async () => {
+    setLoading(true);
+    
     const wsProvider = new WsProvider(RPC_URL_SHIBUYA);
     const api = await ApiPromise.create({provider: wsProvider});
     await api.isReady;
+    
+    const gasLimit = api?.registry.createType('WeightV2', {
+      refTime: MAX_CALL_WEIGHT,
+      proofSize: PROOFSIZE,
+    }) as WeightV2
 
-    // const gasLimit = api.registry.createType('WeightV2', gasRequired) as WeightV2;
-    //
-    // const {gasRequired, result, output} = await nftContract!.query["customMint::mint"]
-    // (account!.address,{
-    //   gasLimit, storageDepositLimit
-    // }, account!.address, input.image, MARKETPLACE_ADDRESS_SHIBUYA);
-    //
-    // const { gasRequired, result, output } =
-    //   await nftContract!.query.get(account!.address, {
-    //     gasLimit,
-    //     storageDepositLimit,
-    //   });
-    //
-    // console.log({ gasRequired, result, output });
-    //
-    // const signer = await web3FromSource(
-    //     account!.meta.source
-    // );
-    //
-    // await nftContract!.tx["customMint::mint"](
-    //     {gasLimit : gasLimit, storageDepositLimit},
-    //     account!.address,
-    //     input.image,
-    //     MARKETPLACE_ADDRESS_SHIBUYA
-    // ).signAndSend(
-    //     account!.address,
-    //     { signer },
-    //     async (res) => {
-    //       if (res.status.isInBlock) {
-    //         console.log('in a block');
-    //       } else if (res.status.isFinalized) {
-    //         console.log('finalized');
-    //       }
-    //     }
-    // );
+    const ImgHash = JSON.stringify(`ipfs://${input.image}`);
+    console.log(ImgHash);
+
+    const accountSigner = await web3FromSource(
+        account!.meta.source
+    );
+
+    await nftContract!.tx["customMint::mint"](
+        {
+          gasLimit,
+          storageDepositLimit,
+        },
+        account!.address,
+        ImgHash,
+        MARKETPLACE_ADDRESS_SHIBUYA
+    ).signAndSend(account!.address, {
+      signer: accountSigner.signer
+    }, async (res) => {
+      if (res.status.isInBlock) {
+        console.log('in a block')
+      } else if (res.status.isFinalized) {
+        console.log('finalized')
+      }
+    });
+
+    setLoading(false);
   }
   
   const walletInit = useCallback(async () => {
@@ -208,6 +204,21 @@ function App() {
               </p>
               {
                 input.image ? (
+                    loading ? (
+                        <div
+                          className={"flex flex-col items-center justify-center w-[526px] h-[73px] mt-[36px] "}
+                        >
+                          <div role="status">
+                            <svg aria-hidden="true"
+                                 className="w-[44px] h-[45px] mr-2 text-[#282828] animate-spin dark:text-gray-600 fill-[#B0B1B7]"
+                                 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                            </svg>
+                            <span className="sr-only">Loading...</span>
+                          </div>
+                        </div>
+                    ) : (
                     <p
                         className={"w-[526px] text-[20px] text-center " +
                             "leading-[25px] text-mono-black font-medium bg-mono-white rounded-[16px] mt-[36px] " +
@@ -215,22 +226,20 @@ function App() {
                     >
                       {input.image}
                     </p>
+                    )
                 ) : (ipfsUploading ? (
-                    <div 
-                        role="status"
-                        className="flex items-center justify-center w-[526px] h-[100px] mt-[36px]"
+                    <div
+                        className={"flex flex-col items-center justify-center w-[526px] h-[73px] mt-[36px] "}
                     >
-                      <svg aria-hidden="true"
-                           className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                           viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                            fill="currentColor"/>
-                        <path
-                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                            fill="currentFill"/>
-                      </svg>
-                      <span className="sr-only">Loading...</span>
+                      <div role="status">
+                        <svg aria-hidden="true"
+                             className="w-[44px] mt-[36px] h-[45px] mr-2 text-[#282828] animate-spin dark:text-gray-600 fill-[#B0B1B7]"
+                             viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                      </div>
                     </div>
                 ) : (
                     <div className="flex items-center justify-center w-full">
